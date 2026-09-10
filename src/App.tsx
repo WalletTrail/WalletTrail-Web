4753a17afa3297bb17e98f13dc4b3546f0bd05e6
WalletTrail <wallettrail.team@gmail.com>
2026-09-10 14:59:56 +0200

    Add WalletTrail 101 route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import Home from './pages/Home/Home';
+import WalletTrail101 from './pages/WalletTrail101/WalletTrail101';
 
 export default function App() {
+  const path = window.location.pathname.replace(/\/$/, '') || '/';
+
+  if (path === '/wallettrail-101') {
+    return <WalletTrail101 />;
+  }
+
   return <Home />;
 }
