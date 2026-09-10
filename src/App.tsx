a1d26da61cdb4e6cad36e90911b8aaee83e93786
WalletTrail <wallettrail.team@gmail.com>
2026-09-10 15:46:09 +0200

    Register Security Centre route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Home from './pages/Home/Home';
 import WalletTrail101 from './pages/WalletTrail101/WalletTrail101';
 import Dictionary from './pages/Dictionary/Dictionary';
+import Security from './pages/Security/Security';
 
 export default function App() {
   const path = window.location.pathname.replace(/\/$/, '') || '/';
@@ -11,6 +12,10 @@ export default function App() {
 
   if (path === '/dictionary') {
     return <Dictionary />;
+  }
+
+  if (path === '/security') {
+    return <Security />;
   }
 
   return <Home />;
