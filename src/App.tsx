00ee1f9972325c188fc6a02808d8526a18deec00
WalletTrail <wallettrail.team@gmail.com>
2026-09-10 15:09:15 +0200

    Add dictionary route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Home from './pages/Home/Home';
 import WalletTrail101 from './pages/WalletTrail101/WalletTrail101';
+import Dictionary from './pages/Dictionary/Dictionary';
 
 export default function App() {
   const path = window.location.pathname.replace(/\/$/, '') || '/';
 
   if (path === '/wallettrail-101') {
     return <WalletTrail101 />;
+  }
+
+  if (path === '/dictionary') {
+    return <Dictionary />;
   }
 
   return <Home />;
