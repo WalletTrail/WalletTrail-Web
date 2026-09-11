e8e018cbf32a54967612d9ade810ab534f4247f9
WalletTrail <wallettrail.team@gmail.com>
2026-09-11 17:13:34 +0200

    Apply WalletTrail logo globally

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
 import './styles/mobile-navigation.css';
+import './styles/logo.css';
 
 const root = document.getElementById('root');
 
