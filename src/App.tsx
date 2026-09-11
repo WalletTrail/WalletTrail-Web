ff8e8c78f4b32987f7415e8387df79ef37308388
WalletTrail <wallettrail.team@gmail.com>
2026-09-11 04:23:08 +0200

    Add Knowledge and Evidence routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import Home from './pages/Home/Home';
 import WalletTrail101 from './pages/WalletTrail101/WalletTrail101';
 import Dictionary from './pages/Dictionary/Dictionary';
 import Security from './pages/Security/Security';
+import Knowledge from './pages/Knowledge/Knowledge';
+import Evidence from './pages/Evidence/Evidence';
 
 const routes = {
   '/wallettrail-101': WalletTrail101,
   '/dictionary': Dictionary,
   '/security': Security,
+  '/knowledge': Knowledge,
+  '/evidence': Evidence,
 } as const;
 
 export default function App() {
